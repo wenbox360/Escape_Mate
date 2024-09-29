7902b2fbf31e42dc8847eb009055de47be1bc419
wenbox360 <101437759+wenbox360@users.noreply.github.com>
2024-09-28 20:54:49 -0400

    Added tts code and very simple game

--- audio/tts.py
+++ audio/tts.py
@@ -1,9 +1,50 @@
-def trigger_tts_for_introductory_narration():
-    print("Triggering TTS for introductory narration...") # Placeholder
+from cartesia import Cartesia
+import pyaudio
+import os
 
-def trigger_tts_for_final_narration():
-    print("Triggering TTS for introductory narration...") # Placeholder
+def tts(user_transcript):
+    client = Cartesia(api_key=os.environ.get("CARTESIA_API_KEY"))
+    voice_name = "Barbershop Man"
+    voice_id = "2461f054-b556-48f5-bc6d-362232d335ab"
+    voice = client.voices.get(id=voice_id)
+    transcript = user_transcript
 
-def trigger_tts_for_charSacters(resistance_narration, shadow_narration, puzzle_state):
-    print(f"Resistance Leader: {resistance_narration}")
-    print(f"Shadow: {shadow_narration}")
+    # You can check out our models at https://docs.cartesia.ai/getting-started/available-models
+    model_id = "sonic-english"
+
+    # You can find the supported `output_format`s at https://docs.cartesia.ai/reference/api-reference/rest/stream-speech-server-sent-events
+    output_format = {
+        "container": "raw",
+        "encoding": "pcm_f32le",
+        "sample_rate": 22050,
+    }
+
+    p = pyaudio.PyAudio()
+    rate = 22050
+
+    stream = None
+
+    # Set up the websocket connection
+    ws = client.tts.websocket()
+
+    # Generate and stream audio using the websocket
+    for output in ws.send(
+        model_id=model_id,
+        transcript=transcript,
+        voice_embedding=voice["embedding"],
+        stream=True,
+        output_format=output_format,
+    ):
+        buffer = output["audio"]
+
+        if not stream:
+            stream = p.open(format=pyaudio.paFloat32, channels=1, rate=rate, output=True)
+
+        # Write the audio data to the stream
+        stream.write(buffer)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    ws.close()  # Close the websocket connection
---
+++ sensors/ai_bias_game/game.py
@@ -0,0 +1,24 @@
+import serial
+from time import sleep
+
+
+
+def game():
+    while True:
+        ser = serial.Serial ("/dev/ttyAMA0", 9600)    #Open port with baud rate
+        received_data = ser.read()              #read serial port
+        sleep(0.03)
+        data_left = ser.inWaiting()             #check for remaining byte
+        received_data += ser.read(data_left)
+        print (received_data)                   #print received data
+        ser.write(received_data)                #transmit data serially 
+
+
+        LED1_status = received_data[0]
+        LED2_status = received_data[1]
+        LED3_status = received_data[2]
+        LED4_status = received_data[3]
+        LED5_status = received_data[4]
+
+        if LED1_status == '4' and LED2_status == '3' and LED3_status == '5' and LED4_status == '1' and LED5_status == '2':  
+            return True 
